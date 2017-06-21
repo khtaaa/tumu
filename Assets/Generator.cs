55ce1a3f74bac9e53ca5ad0e2627d5f34b4b90cf
khtaaa <kahata1128@gmail.com>
2017-06-21 16:15:37 +0900

    プロジェクト作成

---
+++ Assets/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+	public GameObject[] obj;
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;//秒を獲得
+
+		if (time > 3) {
+			time = 0;
+			Instantiate (obj[(int)Random.Range(0,obj.Length)], new Vector3 (-5, 3, 0), Quaternion.identity);
+		}
+	}
+}
---
+++ Assets/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+	public float move;
+	public float rote;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rote = Input.GetAxis ("Vertical");
+		move = Input.GetAxis("Horizontal");
+
+		transform.Rotate (0f, 0f, rote * 1.5f);
+		transform.position+=new Vector3 (move*0.1f, 0f, 0f);
+
+		if (transform.position.x < -6)
+			transform.position = new Vector3 (-6f, -2f, 0f);
+
+		if (transform.position.x > -3)
+			transform.position = new Vector3 (-3f, -2f, 0f);
+	}
+}
